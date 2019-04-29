35a5fe1a0b21ecaa020da0ace0621ad26f79aafc
Thomas <thomaslee9474@gmail.com>
2019-04-29 11:20:39 -0700

    [update] routes

--- controllers/list.route.js
+++ controllers/list.route.js
@@ -10,6 +10,19 @@ router.get('/', parcel(controller.Index));
 // POST: LIST
 router.post('/lists', parcel(controller.CreateList));
 
-...
+// PUT: UPDATE LIST
+router.put('/list/:id', parcel(controller.UpdateList))
+
+// GET: INDIVIDUAL LIST
+router.get('/list/:id', parcel(controller.GetList));
+
+// PUT: UPDATE INDIVIDUAL LIST
+router.put('/list/:id/:list_index', parcel(controller.UpdateIndividualList));
+
+// PUT: UPDATE CHECKBOX
+router.put('/toggled_check/:id/:list_index', parcel(controller.UpdateCheckbox));
+
+// DELETE: LIST
+router.delete('/list/:id', parcel(controller.DeleteList));
 
 module.exports = router;
